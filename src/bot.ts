11ef9b5ea3dae3d917a5aad7e73dd92cdc38e4eb
lucascmpus <lucascmpus@gmail.com>
2023-04-18 19:44:44 -0300

    feat: create random advice command

--- src/bot.ts
+++ src/bot.ts
@@ -1,47 +1,27 @@
-import tmi from 'tmi.js';
-import dotenv from 'dotenv';
 import axios from 'axios';
+import client from './client/tmi';
+import { RandomAdvice } from './types';
 
-dotenv.config();
-
-interface PostData {
-  userId: number;
-  id: number;
-  title: string;
-  body: string;
-}
-
-let data: PostData[] = [];
-
-// Create a client with our options
-const client = new tmi.client({
-  options: {
-    clientId: process.env.ID_CLIENT,
-    debug: true,
-  },
-  identity: {
-    username: process.env.BOT_USERNAME,
-    password: process.env.OAUTH_TOKEN,
-  },
-  channels: [process.env.CHANNEL_NAME || 'k1nhz'],
-});
-
-// Register our event handlers (defined below)
 client.on('message', async (target, context, msg, self) => {
   if (self) {
     return;
   }
 
-  if (msg.trim() === '!tiktok') {
-    await axios
-      .get('https://jsonplaceholder.typicode.com/posts')
-      .then(res => (data = res.data));
-    console.log(data);
+  if (msg.trim() === '!advice') {
+    let message: RandomAdvice = {} as RandomAdvice;
 
-    let option = data[1]['title'];
-    client.say(target, JSON.stringify(option)).catch(err => console.log(err));
+    await axios.get('https://api.adviceslip.com/advice').then(res => {
+      const { slip } = res.data;
+      message = slip.advice;
+    });
+
+    client.say(target, JSON.stringify(message)).catch(err => console.log(err));
+  }
+
+  if (msg.trim() === '!tiktok') {
   }
 });
+
 client.on('connected', (addr, port) => {
   console.log(`* Connected to ${addr}:${port}`);
 });
